94e4482e7072445d10ff9aebe07e43893885803b
xiaohu <xiaohu@qq.com>
2021-12-03 23:18:09 +0800

    First testGit

--- Testgit/src/main/java/com/lichenghu/testGit.java
+++ Testgit/src/main/java/com/lichenghu/testGit.java
@@ -4,6 +4,7 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("hello testGit ...");
         System.out.println("First testGit ...");
+        System.out.println("Second testGit ...");
 
     }
 }
